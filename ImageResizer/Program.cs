6cd273769f9558150982831589005338df47488e
esthersky <esthersky1119@gmail.com>
2019-09-12 11:53:48 +0800

    增加時間計算

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -15,17 +15,26 @@ namespace HomeWork
 
             imageProcess.Clean(destinationPath);
 
+            decimal test1 = 0;
+            decimal test2 = 0;
+
             Stopwatch sw = new Stopwatch();
             sw.Start();
             imageProcess.Pr_ResizeImages(sourcePath, destinationPath, 2.0);
             sw.Stop();
-            Console.WriteLine($"原本，執行花費時間: {sw.ElapsedMilliseconds} ms");
+            test1 = sw.ElapsedMilliseconds;
+            Console.WriteLine($"原本，執行花費時間: {test1} ms");
 
             Stopwatch sw2 = new Stopwatch();
             sw2.Start();
             imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
             sw2.Stop();
-            Console.WriteLine($"後來，執行花費時間: {sw2.ElapsedMilliseconds} ms");
+            test2 = sw2.ElapsedMilliseconds;
+            Console.WriteLine($"後來，執行花費時間: {test2} ms");
+
+            decimal rtp = Math.Round((test2 / test1) * 100, 2);
+
+            Console.WriteLine($"時間減少百分比: {rtp}");
 
             Console.ReadKey();
         }
